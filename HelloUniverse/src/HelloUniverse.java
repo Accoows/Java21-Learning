f27c39b24196644b0fd2feaae661046ac08354c7
Accoow <gnzlsarthur@gmail.com>
2024-04-17 17:50:27 +0200

    Update 1.0

--- HelloUniverse/src/HelloUniverse.java
+++ HelloUniverse/src/HelloUniverse.java
@@ -7,8 +7,10 @@ public class HelloUniverse {
     public static void main(String... args){
         System.out.println("Hello Universe!");
         short ageDuCapitaine = 45;
+        long testTropLong = 45121556611321L;
         float testDeFloat = 78.7F;
         System.out.println(ageDuCapitaine);
         System.out.println(testDeFloat);
+        System.out.println(testTropLong);
     }
 }
