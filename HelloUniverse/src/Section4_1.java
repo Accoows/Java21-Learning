b82bad76e13ae76dae714ff8e83adf745ff05c78
Accoow <gnzlsarthur@gmail.com>
2024-04-22 13:23:30 +0200

    Update Section4_1

---
+++ HelloUniverse/src/Section4_1.java
@@ -0,0 +1,34 @@
+public class Section4_1 {
+    public static void  main(String... args){
+        float resultat = 5f/(2+3);
+        boolean Comparaison = 10 < 10;
+        System.out.println(resultat);
+        System.out.println(Comparaison);
+        System.out.println();
+
+        boolean bool1_1 = true;
+        boolean bool2_1 = true;
+        boolean test1 = bool1_1 & bool2_1;
+        boolean test1_1 = bool1_1 && bool2_1;
+        System.out.println(test1);
+        System.out.println(test1_1);
+
+        /*
+        AND (&):                                OR (|):
+        bool1   bool2   bool1&bool2             bool1   bool2   bool1 | bool2
+        true    true    true                    true    true    true
+        true    false   false                   true    false   true
+        false   true    false                   false   true    true
+        false   false   false                   false   false   false*
+
+        XOR (^):                                NOT (!):
+        bool1   bool2   bool1^bool2             bool1   bool2   !bool1 | bool2
+        true    true    false                   true    true    true
+        true    false   true                    true    false   false (true)
+        false   true    true                    false   true    true
+        false   false   false                   false   false   true (false)
+        * */
+
+
+    }
+}
---
+++ HelloUniverse/src/bureau_de_change.java
@@ -0,0 +1,10 @@
+public class bureau_de_change {
+
+    public static void main(String... args){
+
+        short montant_inital = 1500;
+        float taux_de_change = 1.22f;
+        float conversion = montant_inital * taux_de_change;
+        System.out.println(conversion);
+    }
+}
