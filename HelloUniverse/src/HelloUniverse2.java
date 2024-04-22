079aee5ec6355fd1904b1487c720b02714af2650
Accoow <gnzlsarthur@gmail.com>
2024-04-22 12:07:11 +0200

    Update Exo2.2

---
+++ HelloUniverse/src/HelloUniverse2.java
@@ -0,0 +1,17 @@
+/*
+Exercice de codage 2 : Le cas de pluton Version 2
+ */
+
+public class HelloUniverse2 {
+    public static void main(String... args){
+        String planetestring = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de :";
+        int systemeSolaire = 8;
+        System.out.println(planetestring);
+        System.out.println(systemeSolaire);
+        planetestring = "Il y a quelques années cependant, elles étaient au nombre de : ";
+        systemeSolaire = 9;
+        System.out.println(planetestring);
+        System.out.println(systemeSolaire);
+    }
+}
+
