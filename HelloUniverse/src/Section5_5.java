db062b64e00d55a98593849b3d28f3b565e731b1
Accoow <gnzlsarthur@gmail.com>
2024-04-24 18:08:48 +0200

    Add Sect5_5

---
+++ HelloUniverse/src/Section5_5.java
@@ -0,0 +1,10 @@
+public class Section5_5 {
+
+    public static void main(String... args){
+        int nb = 0;
+
+        for (;nb < 3; nb++) {
+            System.out.println("Salut");
+        }
+    }
+}
---
+++ HelloUniverse/src/Section5_Universe5.java
@@ -0,0 +1,26 @@
+/*
+Exercice de codage 10 : La structure itérative avec le mot clé for
+*/
+
+public class Section5_Universe5 {
+
+    public static void main(String... args){
+
+        for (int systemeSolaire = 7 ; systemeSolaire < 10 ; systemeSolaire++) {
+            switch (systemeSolaire) {
+                case 7:
+                    System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+                    break;
+                case 8:
+                    System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+                    break;
+                case 9:
+                    System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
+                    break;
+                default:
+                    System.out.printf("Le programme ne peut pas fournir de résultat pour %d", systemeSolaire);
+                    System.out.println();
+            }
+        }
+    }
+}
