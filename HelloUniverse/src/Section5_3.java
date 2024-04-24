7a17b02ea3331da959bffbfba26a410453cf6482
Accoow <gnzlsarthur@gmail.com>
2024-04-24 16:09:00 +0200

    Add Section5_3

---
+++ HelloUniverse/src/Section5_3.java
@@ -0,0 +1,18 @@
+public class Section5_3 {
+
+    public static void main(String... args){
+
+        int age = 45;
+        System.out.printf("Mon age est %d ans.", age);
+
+        if (age >= 30) {
+            System.out.println(" Oui, je fais plus jeune.");
+        }
+        else {
+            if (age < 10) {
+                System.out.println(" Bravo à toi de commencer si jeune.");
+            }
+
+        }
+    }
+}
---
+++ HelloUniverse/src/Section5_Universe3.java
@@ -0,0 +1,32 @@
+/*
+Exercice de codage 7 : Utiliser if et else
+*/
+
+public class Section5_Universe3 {
+
+    public static void main(String... args){
+
+        String phraseIntro = "En %d, les planètes du système solaire étaient au nombre de : %d";
+        int systemeSolaire;
+        short annee = 2005;
+
+        if (annee < 1600 || annee > 2020) {
+            System.out.printf("Le programme ne peut pas fournir de résultat pour l'année %d", annee);
+        }
+        else {
+            if (annee < 1700) {
+                systemeSolaire = 7;
+            }
+            else if (annee < 1800) {
+                systemeSolaire = 8;
+            }
+            else if (annee < 2006) {
+                systemeSolaire = 9;
+            }
+            else {
+                systemeSolaire = 8;
+            }
+            System.out.printf(phraseIntro,annee,systemeSolaire);
+        }
+    }
+}
